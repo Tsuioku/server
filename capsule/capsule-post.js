462a0768d71eb749eb3c6e6b9a832b224263fb11
shinbunbun <takamuneyuto@gmail.com>
2020-05-26 19:04:24 +0900

    add POST

---
+++ capsule/capsule-post.js
@@ -0,0 +1,41 @@
+exports.main = async (event) => {
+  const AWS = require('aws-sdk');
+  const dynamoDocument = new AWS.DynamoDB.DocumentClient();
+
+  const data = JSON.parse(event.body);
+  // const type = data.type;
+  const ID = data.id;
+  const memory = data.memory;
+
+  const UUID = uuidv4().split('-').join('');
+
+  const param = {
+    TableName: 'omoideCapsuleTable',
+    Item: {
+      ID: ID,
+      UUID: UUID,
+      memory: memory
+    }
+  };
+  await new Promise((resolve) => {
+    dynamoDocument.put(param, (err, data) => {
+      if (err) {
+        console.log(err);
+        throw new Error(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': 'http://localhost:8080',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: ''
+  };
+  return response;
+
+};
